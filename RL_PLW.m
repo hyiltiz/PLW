da1c3387ff375fe9196828aa61d3052149b88920
HTY <HTY@e0cee43a-2a02-9b40-aaf6-dc03feaa7d10>
2013-03-02 08:18:04 +0000

    Always use single speed.
    git-svn-id: svn://localhost/Users/HTY/Desktop/composition-hyiltiz/PLW.svn/branches/in-out-PLW@26 e0cee43a-2a02-9b40-aaf6-dc03feaa7d10

--- RL_PLW.m
+++ RL_PLW.m
@@ -233,20 +233,21 @@ function RL_PLW(conf, mode)
       end
       flow.restcount = restBetweenTrial(flow.restcount, conf.resttime, conf.restpertrial, w, render.wsize, mode.debug_on, mode.english_on, render.kb, 0, mode.tactile_on);
       WaitSecs(conf.waitBetweenTrials);  % wait black screen between Trials, random
-
+      
       if mode.regenerate_on
-        data.initPosition = Randi(round(data.loopPeriod/4),[2 1]);
-        %             data.paceRate = Randi(2,[2 1]);
-        data.paceRate = repmat(Randi(2), [2 1]);
-        %   data.Track = 1:round(length(data.dotx));% 2 for accuracy, and data.loopPeriod for period
-        [data.lefttouch, data.righttouch] = touchground(data.dotx0, data.initPosition(1), data.paceRate(1), data.Track);     %for the index when PLW touches ground
+          data.initPosition = Randi(round(data.loopPeriod/4),[2 1]);
+          %         data.paceRate = repmat(Randi(2), [2 1]);
+          % Remember not to use those quick ones
+          data.paceRate = [1; 1];
+          %   data.Track = 1:round(length(data.dotx));% 2 for accuracy, and data.loopPeriod for period
+          [data.lefttouch, data.righttouch] = touchground(data.dotx0, data.initPosition(1), data.paceRate(1), data.Track);     %for the index when PLW touches ground
       else
-        % do not generate, use the previously saved data(not enabled by default)
-        load RL_PLW_data;
-        %where we have two PLWs, and quicker pace than 3 maybe too quick
-        %             data.paceRate = Randi(2,[2 1]);
-        data.paceRate = repmat(Randi(2), [2 1]);
-        data.initPosition = Randi(round(data.loopPeriod/4),[2 1]);
+          % do not generate, use the previously saved data(not enabled by default)
+          load RL_PLW_data;
+          %where we have two PLWs, and quicker pace than 3 maybe too quick
+          %             data.paceRate = Randi(2,[2 1]);
+          data.paceRate = repmat(Randi(2), [2 1]);
+          data.initPosition = Randi(round(data.loopPeriod/4),[2 1]);
       end
 
       conf.xshift = (round(rand)*2-1) * conf.xshift;
