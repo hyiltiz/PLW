af53b8eeb21e57401bfa65f63d6aabc422ae9cfc
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2013-05-28 17:43:09 +0800

    default is to do exp

--- InOutTask.m
+++ InOutTask.m
@@ -3,13 +3,13 @@ function InOutTask()
 
 
   % time setting vatiables
-  conf.flpi               =  0.02;        % each frame is set to 20ms (the monitor's flip interval is 16.7ms)
-  conf.trialdur           =  7;          % duration time for every trial
+  conf.flpi               =  0.01;        % each frame is set to 20ms (the monitor's flip interval is 16.7ms)
+  conf.trialdur           =  70;          % duration time for every trial
   conf.repetitions        =  5;           % repetition time of a condition
-  conf.resttime           =  .1;          % rest for 30s
-  conf.restpertrial       =  10;           % every x trial a rest
-  conf.tiltangle          =  5;           % tilt angle for simulating 3D stereo display
-  conf.xshift             =  .25;          % shift PLW for using mirror, see mode.mirror_on
+  conf.resttime           =  30;          % rest for 30s
+  conf.restpertrial       =  1;           % every x trial a rest
+  conf.tiltangle          =  10;          % tilt angle for simulating 3D stereo display
+  conf.xshift             =  .25;         % shift PLW for using mirror, see mode.mirror_on
   conf.shadowshift        = .5;           % distance between PLWs and their twin shadows
   conf.doubleTactileDiff  = 10 ;          % flips between taps on one tactile stimuli (double tactile);0 to disable
 
