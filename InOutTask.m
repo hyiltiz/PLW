6d7028a96bc7f28e97b61ad603197fbcd2299fee
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2013-06-06 15:08:28 +0800

    They overlap

--- InOutTask.m
+++ InOutTask.m
@@ -3,13 +3,13 @@ function InOutTask()
 
 
   % time setting vatiables
-  conf.flpi               =  0.01;        % each frame is set to 20ms (the monitor's flip interval is 16.7ms)
+  conf.flpi               =  0.02;        % each frame is set to 20ms (the monitor's flip interval is 16.7ms)
   conf.trialdur           =  70;          % duration time for every trial
   conf.repetitions        =  5;           % repetition time of a condition
   conf.resttime           =  30;          % rest for 30s
   conf.restpertrial       =  1;           % every x trial a rest
   conf.tiltangle          =  10;          % tilt angle for simulating 3D stereo display
-  conf.xshift             =  .25;         % shift PLW for using mirror, see mode.mirror_on
+  conf.xshift             =  0;         % shift PLW for using mirror, see mode.mirror_on
   conf.shadowshift        = .5;           % distance between PLWs and their twin shadows
   conf.doubleTactileDiff  = 10 ;          % flips between taps on one tactile stimuli (double tactile);0 to disable
 
