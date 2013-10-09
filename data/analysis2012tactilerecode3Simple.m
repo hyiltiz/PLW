f33e78076e2dd71bc1aa59f6f772428134d2f6f3
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2013-10-09 22:38:44 +0800

    day 1&2 subs list

--- data/analysis2012tactilerecode3Simple.m
+++ data/analysis2012tactilerecode3Simple.m
@@ -18,7 +18,7 @@ close all;
 time_on = 1;  % 0: do NOT plot for response time
 encode_on = 1; % encoding for resp type; always set this to 1
 
-subs = {'cuihaoMirrorD_simple_08-Oct-2013.mat','diaoruochenMirrorD_simple_08-Oct-2013.mat','eparMirrorD_simple_ColorBalance_29-Sep-2013.mat','huohuifangMirrorD_simple_08-Oct-2013.mat','litianyiMirrorD_simple_ColorBalance_08-Oct-2013.mat','liuzhaoMirrorD_simple_ColorBalance_08-Oct-2013.mat','liyawenMirrorD_simple_08-Oct-2013.mat','luobinfengMirrorD_simple_09-Oct-2013.mat','mesudeMirrorD_simple_ColorBalance_29-Sep-2013.mat','wangzhongruiMirrorD_simple_08-Oct-2013.mat','xulihuaMirrorD_simple_08-Oct-2013.mat','yuhongfengMirrorD_simple_08-Oct-2013.mat','zhangzehuaMirrorD_simple_08-Oct-2013.mat','zhaoyapingMirrorD_simple_08-Oct-2013.mat','zhaoyuanMirrorD_simple_08-Oct-2013.mat','zhengmeiMirrorD_simple_08-Oct-2013.mat'};
+subs = {'diaoruochenMirrorD_simple_08-Oct-2013.mat','eparMirrorD_simple_ColorBalance_29-Sep-2013.mat','hujiyingMirrorD_simple_09-Oct-2013.mat','huohuifangMirrorD_simple_08-Oct-2013.mat','litianyiMirrorD_simple_ColorBalance_08-Oct-2013.mat','liuzhaoD_simple_ColorBalance_08-Oct-2013buggy.mat','liuzhaoMirrorD_simple_ColorBalance_08-Oct-2013.mat','liyawenMirrorD_simple_08-Oct-2013.mat','luobinfengMirrorD_simple_09-Oct-2013.mat','mesudeMirrorD_simple_ColorBalance_29-Sep-2013.mat','renshanshanMirrorD_simple_09-Oct-2013.mat','sihongweiMirrorD_simple_09-Oct-2013.mat','wanghuiMirrorD_simple_09-Oct-2013.mat','wanglinMirrorD_simple_09-Oct-2013.mat','wangzhongruiD_simple_08-Oct-2013buggy.mat','wangzhongruiMirrorD_simple_08-Oct-2013.mat','xulihuaMirrorD_simple_08-Oct-2013.mat','yuhongfengMirrorD_simple_08-Oct-2013.mat','zhangzehuaD_simple_08-Oct-2013buggy.mat','zhangzehuaMirrorD_simple_08-Oct-2013.mat','zhaoyapingMirrorD_simple_08-Oct-2013.mat','zhaoyuanMirrorD_simple_08-Oct-2013.mat','zhengmeiMirrorD_simple_08-Oct-2013.mat','zhengqianningMirrorD_simple_09-Oct-2013.mat','zhoupengMirrorD_simple_09-Oct-2013.mat','zhufengfengMirrorD_simple_09-Oct-2013.mat'};  % day 1 and day 2
 
 Dur=[];
 Table=[];% collect response switches
@@ -31,13 +31,13 @@ for isub=1:length(subs)
     idx=find(Trials(:,2)==0);
     Trials(idx,:)=[];  % delete the none-response data.
     Trials(Trials(:,7)==3,:)=[];
-    
+
     %% here set the "congruent" and "incongruent" conditions
     Trials(2:end,12)=diff(Trials(:,2));
     idx=find(Trials(:,12)~=0);
     switchrate=length(idx)/length(Trials);
     %Trials(:,2)=((Trials(:,2)-1)==Trials(:,4))+1; % 1--> incong  2-->cong
-    
+
     if encode_on
         for x=1:length(Trials)
             if Trials(x,1)==1 && Trials(x,7)==1 && Trials(x,2)==1 % tactile lead;initial rightwards motion; right resp
@@ -64,7 +64,7 @@ for isub=1:length(subs)
         end
     end
     %
-    
+
     %     if isub>10
     %     for x=1:length(Trials)
     %         if Trials(x,1)==1 && Trials(x,7)==1 && Trials(x,2)==1 % tactile lead;initial rightwards motion; right resp
@@ -93,11 +93,11 @@ for isub=1:length(subs)
     %
     %
     %
-    
+
     % step 2: normalized phase duration for each subject
     Trials(:,3)=Trials(:,3)/(mean(Trials(:,3)));
     Data=[Data;Trials];
-    
+
     % [normdata g] = grpstats(Data(:,3),{Data(:,12)},{'mean','gname'});
     % for i=1:length(g)
     %     idx = find(Data(:,12)== str2num(g{i})); %find that subject's data
@@ -109,38 +109,38 @@ for isub=1:length(subs)
     %    Trials1=Trials(idx1,:); % red PLW is leftwards motion;
     %    idx2 =find(Trials(:,4)==1);
     %    Trials2=Trials(idx2,:); %  red PLW is rightwards motion;
-    
-    
+
+
     tbl1 = zeros(4,1);
     tbl2 = zeros(4,1);
     if IsOctave
-        
+
         if encode_on
             dur = accumarray([Trials(:,1) Trials(:,11)],Trials(:,3),[],@mean);
         else
             dur = accumarray([Trials(:,1) Trials(:,2)],Trials(:,3),[],@mean);
         end
-        
+
         dur = reshape(dur',[numel(dur),1]);
         g = [repmat(1:size(dur,1),[1 numel(dur)/size(dur,1)])' repmat(1:size(dur,2),[1 numel(dur)/size(dur,2)])'];
         dur = [dur g];
-        
+
         [tbl1v tblidx1]= table(Trials(Trials(:,2)==1,1));%number of incongruent response
         [tbl2v tblidx2]= table(Trials(Trials(:,2)==2,1));%number of congruent response
     else
-        
+
         if encode_on
             [dur g] = grpstats(Trials(:,3),{Trials(:,1),Trials(:,11)},{'mean','gname'}); %  cond: 4; resp: 1-inward; 2-outward
         else
             [dur g] = grpstats(Trials(:,3),{Trials(:,1),Trials(:,2)},{'mean','gname'}); %  cond: 4; resp: 1-inward; 2-outward
         end
-        
-        
+
+
         for j=1:length(g)
             dur(j,2)=str2num(g{j,1});
             dur(j,3)=str2num(g{j,2});
         end
-        
+
         if encode_on
             tbl1x = tabulate(Trials(Trials(:,11)==1,1));
             tbl2x = tabulate(Trials(Trials(:,11)==2,1));
@@ -149,17 +149,17 @@ for isub=1:length(subs)
             tbl1x = tabulate(Trials(Trials(:,2)==1,1));
             tbl2x = tabulate(Trials(Trials(:,2)==2,1));
         end
-        
+
         %         tbl1v = tbl1x(:,2);
         %         tbl2v = tbl2x(:,2);
-        
+
         %         tblidx1 = tbl1x(:,1);
         %         tblidx2 = tbl2x(:,1);
     end
     %     tbl1(tblidx1) = tbl1v;
     %     tbl2(tblidx2) = tbl2v;
-    
-    
+
+
     for j=1:4 % cond
         idxtemp=find(dur(:,2)==j);
         durtemp=dur(idxtemp,:);
@@ -168,10 +168,10 @@ for isub=1:length(subs)
                 durtemp(size(durtemp,1)+1,:) = [0.001 j resp];
             end
         end
-        
+
         Durtemp=[Durtemp;sortrows(durtemp,3)];
     end
-    
+
     dur1=reshape(Durtemp(:,1),[numel(unique(Durtemp(:,3))) numel(Durtemp(:,1))/numel(unique(Durtemp(:,3)))])'; % four column,
     Dur=[Dur;dur1];
     Table = [Table;[tbl1'  tbl2']]; % 1-4 column for incong; 5-8 for cong
@@ -180,7 +180,7 @@ for isub=1:length(subs)
     %    redPLWupinicong=(dur1(:,7)+dur2(:,7))/2;
     %    redPLWinvertcong=(dur1(:,6)+dur2(:,6))/2;
     %    redPLWinvertincong=(dur1(:,8)+dur2(:,8))/2;
-    
+
     figure;
     hold on;
     if time_on
@@ -191,12 +191,12 @@ for isub=1:length(subs)
         plot(1:4, tbl2,'s-.');
         ylabel('number of responses');
     end;
-    
+
     hold off;
     xlabel('tactile conditions');
     set(gca,'Xtick',1:4);
     set(gca,'XtickLabel',{'Incongruent','Congruent','Sync', 'Baseline'});
-    
+
     %print(gcf, '-dpng', ['/scratch/' 'response_time' subs{isub} '.png']);
     %    print(gcf, '-dpng', ['/scratch/' 'switch_rate' subs{isub} '.png']);
 end
@@ -227,4 +227,5 @@ hold off;
 % legend('Incong','Cong');
 xlabel('tactile conditions');
 set(gca,'Xtick',1:4);
-set(gca,'XtickLabel',{'Incongruent','Congruent','Sync', 'Baseline'});
+set(gca,'XtickLabel',{'Incongruent','Congruent','Sync', 'Baseline'});cuihaoMirrorD_simple_08-Oct-2013.mat
+
