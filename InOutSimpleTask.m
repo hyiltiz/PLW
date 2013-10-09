5f2db922ab3acdab4c8db05f3efd615f42fe6425
PLW-410-OldChem <hyiltiz@gmail.com>
2013-10-09 22:28:08 +0800

    InOutSimplw, exp day 2

--- InOutSimpleTask.m
+++ InOutSimpleTask.m
@@ -2,16 +2,16 @@ function InOutSimpleTask(is_once_on)
 % In-Out simple task, pass in 1 for demo with only one trial, or just run it with no input;
 
 
-  % time setting vatiables
-  conf.flpi               =  0.01;        % each frame is set to 20ms (the monitor's flip interval is 16.7ms)
-  conf.trialdur           =  70;          % duration time for every trial
-  conf.repetitions        =  5;           % repetition time of a condition
-  conf.resttime           =  30;          % rest for 30s
-  conf.restpertrial       =  5;           % every x trial a rest
-  conf.tiltangle          =  7;           % tilt angle for simulating 3D stereo display
-  conf.xshift             =  0;           % shift PLW for using mirror, see mode.mirror_on
-  conf.shadowshift        = .4;           % distance between PLWs and their twin shadows
-  conf.doubleTactileDiff  = 10 ;          % flips between taps on one tactile stimuli (double tactile);0 to disable
+% time setting vatiables
+conf.flpi               =  0.01;        % each frame is set to 20ms (the monitor's flip interval is 16.7ms)
+conf.trialdur           =  70;          % duration time for every trial
+conf.repetitions        =  5;           % repetition time of a condition
+conf.resttime           =  30;          % rest for 30s
+conf.restpertrial       =  5;           % every x trial a rest
+conf.tiltangle          =  7;           % tilt angle for simulating 3D stereo display
+conf.xshift             =  0;           % shift PLW for using mirror, see mode.mirror_on
+conf.shadowshift        = .4;           % distance between PLWs and their twin shadows
+conf.doubleTactileDiff  = 10 ;          % flips between taps on one tactile stimuli (double tactile);0 to disable
 
 % state control variables
 mode.simpleInOut_on= 1;  % simple InOut exp, with the same tactile stimuli for both foot
