6f274a5abcd55559359965aac793090aab1f717f
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2014-04-07 23:27:57 +0800

    spacing for StaticChoice

--- lib/oneItem.m
+++ lib/oneItem.m
@@ -1,4 +1,4 @@
-function responseC =  oneItem(ques, i, w, wsize, kb, responseC)
+function [responseC t] =  oneItem(ques, i, w, wsize, kb, responseC)
 scalemap = ['1: ' ques.scales{i, 1}];
 for j=2:size(ques.scales, 2)
     scalemap = [scalemap, '  ', num2str(j) ': ', ques.scales{i, j}];
@@ -8,10 +8,11 @@ DrawFormattedText(w, ques.instr{i}, 0, 80, [255, 255, 255, 255]);
 Screen('DrawText', w, ['请用键盘上数字 1 到 ', num2str(size(ques.scales, 2)), ' 作出反应：'], 0, 300, [255, 255, 255, 255]);
 Screen('DrawText', w, scalemap, 0, 330, [255, 255, 255, 255]);
 
-kbCode = Instruction(ques.items{i}, w, wsize, 0, 1, kb, 5 ,1, 0);
-if sum(kbCode)==0
-    [t, kbCode] = KbWait([],2);
-end
+% kbCode = Instruction(ques.items{i}, w, wsize, 0, 1, kb, 5 ,1, 0);
+DrawFormattedText(w, ques.items{i}, 'center', 450, [255 255 255], 32);
+
+[t, kbCode] = KbWait([],2);
+
 kbName = KbName(kbCode);
 responseC{i} = kbName(1);
 end
