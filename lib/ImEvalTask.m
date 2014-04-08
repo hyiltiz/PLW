8c16e9e33effd36e810f039419a53f022b6c2fc9
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2014-04-08 16:37:49 +0800

    bugfix: input

--- lib/ImEvalTask.m
+++ lib/ImEvalTask.m
@@ -1,4 +1,4 @@
-function wrkspc = ImEvalTask(is_once_on)
+function wrkspc = ImEvalTask(is_once_on, Subinfo)
 % Image evaluation task, pass in 1 for demo with only one trial, or just run it with no input;
 % show face stimuli used by Octal task with same freq/distribution
 % eval from [1-7] ~ [anger-happy]
@@ -20,7 +20,7 @@ conf.clockR             = .5;         % clock, with the center of the screen as 
 % state control variables
 mode.octal_on      = 1;
 mode.RT_on         = 1;  % Reaction time mode, this is not to be changed!
-mode.debug_on      = 0;  % default is 0; 1 is not to use full screen, and skip the synch test
+mode.debug_on      = 1;  % default is 0; 1 is not to use full screen, and skip the synch test
 mode.dotRot_on     = 0;  % Use dot rot or not; depends on octal_on=1;
 mode.colorbalance_on=1;  % balance the color of the target PLW, which is by default red
 mode.mirror_on     = 1;  % use mirror rather that spectacles for binacular rivalry
@@ -32,7 +32,7 @@ if nargin > 0
     mode.once_on       = is_once_on;  % only one trial, used for demostration before experiment
 else
     mode.once_on       = 0;  % only one trial, used for demostration before experiment
-
+    
 end
 
 % Call the main function RL_PLW()
