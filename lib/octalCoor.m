fdc84cfb4a49f25b3475d76573482ef29f330e24
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2014-04-17 14:02:29 +0800

    do not overlap

--- lib/octalCoor.m
+++ lib/octalCoor.m
@@ -5,6 +5,10 @@ function [clockarm, prCoor, angls]= octalCoor(wsize, r, n)
 angl=2*pi/n;
 R=wsize(4)/2;
 angls = angl*[1:n];
+if n==8;
+    adjst = pi/15;
+    angls([1 5]) = angls([1 5]) - adjst;
+    angls([3 7]) = angls([3 7]) + adjst;
 prCoor = [cos(angls)', sin(angls)'];
 
 clockarm = [wsize(3)/2+r.*R.*cos(angls)', wsize(4)/2+r.*R.*sin(angls)'];
